bd38ca6c29099d166f9c00a1d4f93feda1f0c791
devnull9090 <134255848+devnull9090@users.noreply.github.com>
2023-06-24 22:56:45 -0700

    Add a created at and updated at auto value for the codes table
    I added an updated at and created at so I can audit when codes were created and updated.

--- codes.sql
+++ codes.sql
@@ -2,5 +2,7 @@ DROP TABLE IF EXISTS `codes`;
 CREATE TABLE IF NOT EXISTS `codes` (
   `code` varchar(50) NOT NULL DEFAULT '',
   `packagename` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
